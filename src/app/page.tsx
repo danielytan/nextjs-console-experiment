1bbdd6345974955a1a68798ae7e545076358dd31
Daniel <danielytan1@gmail.com>
2023-05-23 13:43:47 +0900

    changed homepage div class to container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Welcome to the Admin Console</h1>
       <Link href="/console" passHref>
         Go to Console
