63a14a2143ff5a664c4f7f3b5af5ab2f9efd18c7
Daniel <danielytan1@gmail.com>
2023-05-23 13:52:36 +0900

    console window now scrolls properly, and looks somewhat like a console

--- src/app/console/page.tsx
+++ src/app/console/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
 
 const ConsolePage = () => {
   const [command, setCommand] = useState('');
   const [output, setOutput] = useState('');
+  const consoleRef = useRef<HTMLDivElement>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCommand(event.target.value);
@@ -17,13 +18,24 @@ const ConsolePage = () => {
     // Process the command and update the output state
     // You can implement your own logic here
 
+    setOutput(prevOutput => prevOutput + '> ' + command + '\n');
+
     // Clear the input field
     setCommand('');
   };
 
+  useEffect(() => {
+    if (consoleRef.current) {
+      consoleRef.current.scrollTop = consoleRef.current.scrollHeight;
+    }
+  }, [output]);
+
   return (
     <div className="container">
       <h1>Console</h1>
+      <div className="console-window" ref={consoleRef}>
+        <pre>{output}</pre>
+      </div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -33,7 +45,6 @@ const ConsolePage = () => {
         />
         <button type="submit">Submit</button>
       </form>
-      <pre>{output}</pre>
       <Link href="/" passHref>
         Go to Home
       </Link>
